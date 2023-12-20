5912d944e4323b8467772ef738c90cff79a05f54
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-20 17:45:13 -0500

    cool

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import styles from "./page.module.css"
 import ContactForm from '@/components/contactForm/ContactForm'
 import Socials from '@/components/socials/Socials'
+import Link from 'next/link'
+import AddressMap from '@/components/addressMap/AddressMap'
 
 export default function Page() {
     return (
         <main>
+            <section>
+                <div>
+                    <p style={{ textAlign: "center" }} className='extraLargeText'>Contact Me</p>
+                </div>
+            </section>
+
             <section style={{ backgroundColor: "var(--backgroundColor)" }}>
                 <div style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", gap: "2rem", padding: "2rem 0" }}>
                     <div className={styles.mainTextArea} style={{ flex: "99999 1 400px" }}>
@@ -27,18 +35,18 @@ export default function Page() {
                                 </li>
 
                                 <li>
-                                    <p>Phone:</p>
-                                    <p>0123456789</p>
+                                    <p>Email:</p>
+                                    <p>quantumbuilder32@gmail.com</p>
                                 </li>
 
                                 <li>
-                                    <p>Phone:</p>
-                                    <p>0123456789</p>
+                                    <p>Website:</p>
+                                    <p><Link href={`https://codemax.vercel.app/`}>codemax.vercel.app</Link></p>
                                 </li>
 
                                 <li>
-                                    <p>Phone:</p>
-                                    <p>0123456789</p>
+                                    <p>Address:</p>
+                                    <p>Kingston, Jamaica</p>
                                 </li>
                             </ul>
                         </div>
@@ -50,6 +58,10 @@ export default function Page() {
                         </div>
                     </div>
                 </div>
+            </section>
+
+            <section>
+                <AddressMap />
             </section>
         </main>
     )
---
+++ components/addressMap/AddressMap.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function AddressMap() {
+    return (
+        <iframe style={{ overflow: "hidden" }} width="100%" height="600" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=X6P6+GR7,%20National%20Heroes%20Cir,%20Kingston+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population mapping</a>
+        </iframe>
+    )
+}

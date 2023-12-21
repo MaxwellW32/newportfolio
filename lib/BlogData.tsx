c6febd51d1e8c7b69509e7985d56391f8ad0b34d
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-21 04:15:32 -0500

    test

--- lib/BlogData.tsx
+++ lib/BlogData.tsx
@@ -16,8 +16,8 @@ export const blogs: blog[] = [
         slug: "html-practice",
         messages: [
             <div key={0}>
-                <p>I've learned so so much in this amzaing language, so many tips and tricks!</p>
-                <p style={{ marginTop: "1rem" }}>As a beginner, I quickly grasped the fundamental structure that HTML provides to a webpage. The 'head' for meta-information, the 'body' for content – it all started to make sense. Embracing semantic HTML not only improved my site&apos;s accessibility but also enhanced my understanding of how search engines interpret content. Remembering to use proper tags and nesting became second nature, laying a strong foundation for my coding endeavors.</p>
+                <p>I&apos;ve learned so so much in this amzaing language, so many tips and tricks!</p>
+                <p style={{ marginTop: "1rem" }}>As a beginner, I quickly grasped the fundamental structure that HTML provides to a webpage. The &apos;head&apos; for meta-information, the &apos;body&apos; for content – it all started to make sense. Embracing semantic HTML not only improved my site&apos;s accessibility but also enhanced my understanding of how search engines interpret content. Remembering to use proper tags and nesting became second nature, laying a strong foundation for my coding endeavors.</p>
             </div>
         ]
     },
@@ -65,7 +65,7 @@ export const blogs: blog[] = [
         slug: "react-practice",
         messages: [
             <div key={0}>
-                <p>React has been the heartbeat of my web development journey, introducing me to the world of declarative and component-based UI design. As a beginner, the concept of breaking down user interfaces into reusable components was a game-changer. The virtual DOM and React's efficient rendering process ensured optimal performance, even for complex applications. One of the key takeaways for me has been the importance of state management. Understanding how to lift state up, use hooks, and manage the application's state flow has been pivotal in creating interactive and responsive user interfaces. React's unidirectional data flow philosophy brought clarity to how data moves through components, making it easier to reason about and debug. Additionally, the vibrant React community and the abundance of open-source libraries have provided me with a wealth of resources and best practices. As I continue to explore React, I find its flexibility and scalability to be essential for crafting modern, dynamic, and maintainable web applications.</p>
+                <p>React has been the heartbeat of my web development journey, introducing me to the world of declarative and component-based UI design. As a beginner, the concept of breaking down user interfaces into reusable components was a game-changer. The virtual DOM and React&apos;s efficient rendering process ensured optimal performance, even for complex applications. One of the key takeaways for me has been the importance of state management. Understanding how to lift state up, use hooks, and manage the application&apos;s state flow has been pivotal in creating interactive and responsive user interfaces. React&apos;s unidirectional data flow philosophy brought clarity to how data moves through components, making it easier to reason about and debug. Additionally, the vibrant React community and the abundance of open-source libraries have provided me with a wealth of resources and best practices. As I continue to explore React, I find its flexibility and scalability to be essential for crafting modern, dynamic, and maintainable web applications.</p>
             </div>
         ]
     },

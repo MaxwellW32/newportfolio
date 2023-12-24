395747d3f91cf87ae52c181b4a62b185602a7488
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-24 11:18:56 -0500

    nice

--- app/projects/downloader/page.tsx
+++ app/projects/downloader/page.tsx
@@ -15,8 +15,6 @@ export default function Page() {
     const [info, infoSet] = useState<ytdl.videoInfo>()
     const [highestFormat, highestFormatSet] = useState<ytdl.videoFormat>()
 
-    const [showingNav, showingNavSet] = useState(false)
-    const [navProperlyHidden, startedSet] = useState(false)
 
     const [viewerPick, viewerPickSet] = useState<"both" | "video only" | "audio only">("both")
 
@@ -37,22 +35,8 @@ export default function Page() {
         console.log(`$info`, seenInfo);
     }
 
-    //hide Navs
-    useEffect(() => {
-        const nav = document.querySelector("nav")!
-        const footer = document.querySelector("footer")!
-
-        nav.classList.add("clear")
-        footer.classList.add("clear")
-        startedSet(true)
-
-        return () => {
-            nav.classList.remove("clear")
-            footer.classList.remove("clear")
-        }
-    }, [])
     return (
-        <div className={`blackFadeIn ${styles.mainDiv}`} style={{ "--animationPlaystate": navProperlyHidden ? "running" : "paused", textAlign: "center", maxHeight: "100svh", display: "grid", gridTemplateRows: "2fr 1fr", gap: "1rem" } as React.CSSProperties}>
+        <div className={styles.mainDiv} style={{ textAlign: "center", maxHeight: "100svh", display: "grid", gridTemplateRows: "2fr 1fr", gap: "1rem" }}>
 
             <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(min(350px, 100%), 1fr))", gap: '1rem', justifyContent: "center", overflow: "auto", padding: "2rem" }}>
                 <div style={{ display: "grid", gridTemplateRows: "auto 1fr" }}>
@@ -182,22 +166,6 @@ export default function Page() {
 
 
             <Image priority={true} alt="background" src={require("@/public/projects/downloader/bg.png")} width={1920} height={1080} style={{ width: "100vw", height: "100svh", position: "absolute", top: 0, left: 0, zIndex: -1, objectFit: "cover", }} />
-
-            <svg xmlns="http://www.w3.org/2000/svg" style={{ position: "fixed", width: "1.5rem", bottom: 0, right: 0, margin: "1rem" }}
-                onClick={() => {
-                    const newShowingNav = !showingNav
-                    showingNavSet(prev => newShowingNav)
-
-                    const nav = document.querySelector("nav")!
-
-                    if (!showingNav) {
-                        nav.classList.remove("clear")
-                    } else {
-                        nav.classList.add("clear")
-                    }
-
-                }}
-                viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" /></svg>
         </div>
     )
 }
---
+++ app/projects/layout.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [showingNav, showingNavSet] = useState(false)
+  const [navProperlyHidden, navProperlyHiddenSet] = useState(false)
+
+  //hide Navs
+  useEffect(() => {
+    const nav = document.querySelector("nav")!
+    const footer = document.querySelector("footer")!
+    const music = document.querySelector(".musicDiv")
+
+    nav.classList.add("clear")
+    footer.classList.add("clear")
+    music?.classList.add("clear")
+    navProperlyHiddenSet(true)
+
+    return () => {
+      nav.classList.remove("clear")
+      footer.classList.remove("clear")
+      music?.classList.remove("clear")
+    }
+  }, [])
+
+  return (
+    <div className="blackFadeIn" style={{ "--animationPlaystate": navProperlyHidden ? "running" : "paused" } as React.CSSProperties}>
+      {children}
+
+      <svg xmlns="http://www.w3.org/2000/svg" style={{ position: "fixed", width: "1.5rem", bottom: 0, right: 0, margin: "1rem", zIndex: 999 }}
+        onClick={() => {
+          const newShowingNav = !showingNav
+          showingNavSet(prev => newShowingNav)
+
+          const nav = document.querySelector("nav")!
+          const music = document.querySelector(".musicDiv")!
+
+
+          if (!showingNav) {
+            nav.classList.remove("clear")
+            music.classList.remove("clear")
+          } else {
+            nav.classList.add("clear")
+            music.classList.add("clear")
+
+          }
+
+        }}
+        viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" /></svg>
+
+    </div>
+  )
+}

9b35d9853d18ce04f795a6e32104c0478aba5d48
MaxwellW32 <maxwellwedderburn32@gmail.com>
2024-01-13 02:13:06 -0500

    new

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -67,11 +67,11 @@ export default function Page() {
             points: 9
         },
         "king": {
-            piece: "queen",
+            piece: "king",
             points: 0
         },
         "pawn": {
-            piece: "queen",
+            piece: "pawn",
             points: 1
         },
     }
@@ -230,7 +230,7 @@ export default function Page() {
         if (checkmatedKing) clearInterval(autoPlayLoop.current)
 
         autoPlayLoop.current = setInterval(() => {
-            play(chessPieces)
+            // play(chessPieces)
             autoMovingSet(true)
         }, 800)
 
